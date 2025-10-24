3ada53541d8e8d0214c5ed6b62f97147317eb27a
Fabio0 <joakobar2000@gmail.com>
2025-10-23 23:42:26 -0300

    END OF section 6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { MemberDetailed } from '../Features/members/member-detailed/member-detailed';
+import { Home } from '../Features/home/home';
+import { MemberList } from '../Features/members/member-list/member-list';
+import { Lists } from '../Features/lists/lists';
+import { Messages } from '../Features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '',component: Home},
+    {path: 'member', component: MemberList, canActivate: [authGuard]},
+    {
+        path: '',
+        runGuardsAndResolvers: "always",
+        canActivate: [authGuard],
+        children: [
+            {path: 'member', component: MemberList},
+            {path: 'members/:id', component: MemberDetailed},
+            {path: 'lists', component: Lists},
+            {path: 'messages', component: Messages},
+        ]
+    },
+    {path: '**', component: Home},
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
+import { Router, RouterOutlet } from '@angular/router';
+import { Nav } from './layout/nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
+
+export class App{
+  protected router = inject(Router);
   private http = inject(HttpClient);
   protected title = "Dating App";
-  protected members = signal<any>([])
 
-  async ngOnInit() {
-    this.members.set(await this.getMembers())
-  }
-
-  async getMembers() {
-    try{
-      return lastValueFrom(this.http.get("https://localhost:5001/api/members"));
-    } catch(error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
