95b4ada55b83e2f6deef428a77c073b9cb9df5b8
Fabio0 <joakobar2000@gmail.com>
2025-11-10 20:25:36 -0300

    End of Section 12

--- src/types/user.ts
+++ src/types/user.ts
@@ -15,4 +15,9 @@ export type RegisterCreds = {
     email: string;
     displayname: string;
     password: string;
+    gender: string;
+    dateOfBirth: string;
+    city: string;
+    country: string;
 }
+
