74c341f53734ca3c7e7822fdd261a60585653180
Fabio0 <joakobar2000@gmail.com>
2025-11-11 02:24:44 -0300

    End of Section 13

--- src/core/interceptor/loading-interceptor.ts
+++ src/core/interceptor/loading-interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpInterceptorFn, HttpParams } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { delay, finalize, of, tap } from 'rxjs';
 import { BusyService } from '../services/busy-service';
@@ -8,17 +8,25 @@ const cache = new Map<string, HttpEvent<unknown>>();
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService);
 
+  const generateCacheKey = (url: string, params: HttpParams): string =>{
+    const paramString = params.keys().map(key => `${key}=${params.get(key)}`).join('&');
+    return paramString ? `${url}?${paramString}` : url;
+  }
+
+  const cacheKey = generateCacheKey(req.url, req.params);
+  
   if(req.method === "GET"){
-    const cachedResponse = cache.get(req.url);
+    const cachedResponse = cache.get(cacheKey);
     if(cachedResponse){
       return of(cachedResponse);
     }
   }
+
   busyService.busy();
   return next(req).pipe(
     delay(500),
     tap(Response => {
-      cache.set(req.url, Response)
+      cache.set(cacheKey, Response)
     }),
     finalize(() => {
       busyService.idle()
